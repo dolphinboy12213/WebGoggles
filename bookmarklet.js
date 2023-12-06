63ca83896a22da18f42ff5e07a5b72007a5ef472
TheDolphin <dolphinboy12213@gmail.com>
2023-12-06 11:52:13 -0600

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function%20()%20%7Bvar%20script=document.createElement('script');script.src='https://x-ray-goggles.mouse.org/webxray.js';script.className='webxray';script.setAttribute('data-lang','en-US');script.setAttribute('data-baseuri','https://x-ray-goggles.mouse.org');document.body.appendChild(script);%7D())
